d529683e6375bb8df210751f72e174c7c0202835
javadroid <ochigboemmanuel32@gmail.com>
2022-11-14 15:00:58 +0100

    readme

--- src/components/WalletConnectButton.js
+++ src/components/WalletConnectButton.js
@@ -10,7 +10,6 @@ import { useEffect } from "react";
  * @author [Javadroid](https://github.com/javadroid)
  */
 
-
 export const WalletConnectButton = () => {
   /**
    *
@@ -19,7 +18,7 @@ export const WalletConnectButton = () => {
    * openAuthRequest, signOut, along with some helper variables: isRequestPending isSignedIn.
    *
    */
-
+  const [res, setRes] = useState(false);
   const { openAuthRequest, isRequestPending, signOut, isSignedIn } = useAuth();
   const label = isRequestPending
     ? "Loading..."
@@ -27,18 +26,26 @@ export const WalletConnectButton = () => {
     ? "Sign out"
     : "Connect Stacks wallet";
 
-
+    const url= res ? <p>no Stacks wallet available please download <a style={{color:'red'}} href="https://wallet.hiro.so/">Hiro Wallet</a> </p> : <a href="">asasd</a>
   return (
     <>
-     
       <button
         onClick={async () => {
-          if (isSignedIn) await signOut()
-          else await openAuthRequest();
+          if (isSignedIn) await signOut();
+          else
+            await openAuthRequest().catch((err) => {
+              if(  err.message.includes("there is no Stacks wallet available")){
+
+                setRes(err.message.includes("there is no Stacks wallet available"))
+              }
+             
+            });
         }}
       >
         {label}
       </button>
+
+      {url}
     </>
   );
 };
