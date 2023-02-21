ae21e0f9cee390a2e4567bee4ab187a77ff29b66
bloodfuse <109753369+bloodfuse@users.noreply.github.com>
2023-02-21 12:34:14 +0100

    Update KnowOurPatients.jsx

--- src/components/LandingPage/KnowOurPatients.jsx
+++ src/components/LandingPage/KnowOurPatients.jsx
@@ -8,30 +8,30 @@ import "swiper/css/navigation";
 const KnowOurPatients = () => {
   const persons = [
     {
-      name: "Oluwole Seun",
+     // name: "Oluwole Seun",
       videoSrc: "",
-      details:
+     // details:
         "Meet 14 years old Seun from Lagos. She and others who are fighting Leukemia often need blood trans-fusions while going through chemo treatments.",
       btnDescription: "Watch on Our Youtube",
-      youtubeLink: "https://www.youtube.com/embed/JU5ZoGU3wzA",
+      youtubeLink: "https://youtu.be/1Vb6srw5bR8",
     },
-    {
-      name: "Audu Haruna",
-      videoSrc: "",
-      details:
-        "Meet 28 years old Haruna from Kano, who is fighting Cancer often need blood trans-fusions while going through chemo treatments.",
-      btnDescription: "Watch on Our Youtube",
-      youtubeLink: "https://www.youtube.com/embed/fehAgOqTR44",
-    },
+//     {
+//       name: "Audu Haruna",
+//       videoSrc: "",
+//       details:
+//         "Meet 28 years old Haruna from Kano, who is fighting Cancer often need blood trans-fusions while going through chemo treatments.",
+//       btnDescription: "Watch on Our Youtube",
+//       youtubeLink: "https://www.youtube.com/embed/fehAgOqTR44",
+//     },
   ];
   return (
     <section className="w-full pb-6 md:pb-14 h-auto">
       <h1 className="text-2xl md:text-3xl text-center pb-16 px-2 font-bold hidden sm:block">
-        GET TO KNOW OUR PATIENTS MORE
+        How To Create An Account on Bloodfuse
       </h1>
-      <h1 className="text-2xl md:text-3xl text-center py-12 px-2 font-bold block sm:hidden">
-        GET TO KNOW OUR PATIENTS
-      </h1>
+//       <h1 className="text-2xl md:text-3xl text-center py-12 px-2 font-bold block sm:hidden">
+//         GET TO KNOW OUR PATIENTS
+//       </h1>
       {/* The Swiper Component */}
       <Swiper
         slidesPerView={1}
